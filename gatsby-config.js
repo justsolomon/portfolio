d9279e2cdff050b22187fb17de4896cda5bd0665
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-12-17 08:14:22 +0100

    Remove unnecessary css styles with purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,26 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Gbolahan Balogun - Portfolio',
+        short_name: 'Gbolahan Balogun',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true,
+        develop: true,
+      },
+    },
     'gatsby-plugin-robots-txt',
   ],
 };
