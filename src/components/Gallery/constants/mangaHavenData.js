2b8515f1b655a6b599e4fa6a46ddb3ad7ed09858
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2023-07-13 01:29:47 -0600

    update mangahaven data

--- src/components/Gallery/constants/mangaHavenData.js
+++ src/components/Gallery/constants/mangaHavenData.js
@@ -3,8 +3,8 @@ import full01 from '../../../assets/images/mangahaven/full01.png';
 
 const mangaHavenData = {
   name: 'MangaHaven',
-  description: `A task management web application with synchronization across multiple devices.`,
-  skills: ['TypeScript', 'React', 'Redux', 'Node.js'],
+  description: `An online manga reader.`,
+  skills: ['CSS3', 'JavaScript', 'React'],
   sources: [
     { caption: "MangaHaven Demo", source:"https://user-images.githubusercontent.com/55158465/253188295-4a84df32-0f57-4869-a429-6c4ee3b128b8.gif" },
     { caption: 'Google Search results for \'mangahaven\'', source: full01 },
